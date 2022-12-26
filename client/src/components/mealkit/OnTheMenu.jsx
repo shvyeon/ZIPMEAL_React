f278ff77d00351eff4e113398dc5db7b997ca074
shvyeon <gustjsgp8569@naver.com>
2022-12-26 20:45:17 +0900

    router setting

--- client/src/components/mealkit/OnTheMenu.jsx
+++ client/src/components/mealkit/OnTheMenu.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function OnTheMenu() {
-  return <div></div>;
+  return <div>on-the-menu</div>;
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LogIn from "./components/general/LogIn";
+import SignUp from "./components/general/SignUp";
+import OnTheMenu from "./components/mealkit/OnTheMenu";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const rootElement = document.getElementById("root");
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/log-in" element={<LogIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/mealkit/onTheMenu" element={<OnTheMenu />} />
+    </Routes>
+  </BrowserRouter>,
+  rootElement
 );
 
 // If you want to start measuring performance in your app, pass a function
